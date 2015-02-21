249bf33afadbae469631112af4c5d592f1990367
Srdjan Tubin <stubin87@gmail.com>
2015-02-21 14:05:07 +0100

    add and remove exp type

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,14 +1,16 @@
 angular.module('ratedExpApp', [])
     .controller('MainController', ['$scope', function($scope) {      
+        var maxId = 4;
+        
         $scope.expTypes = 
         [
             {
                 "id": 1,
-                "name": {"value": "Beer", "type": "text"},
+                "name": {"value": "Beer", "type": "text"}
             },
             {
                 "id": 2,
-                "name": {"value": "Cities", "type": "text"},
+                "name": {"value": "Cities", "type": "text"}
             },
             {
                 "id": 3,
@@ -16,6 +18,22 @@ angular.module('ratedExpApp', [])
             }
         ];
         
+        $scope.addNewExpType = function(newExpTypeName) {
+            $scope.expTypes.push({"id": maxId, "name": {"value": newExpTypeName, "type": "text"}});
+            $scope.newExpTypeName = null;
+        };
         
+        $scope.deleteExpType = function(expTypeId) {            
+            findAndRemove($scope.expTypes, 'id', expTypeId);
+        };
     }]);
 
+function findAndRemove(array, property, value) {
+   $.each(array, function(index, result) {
+      if(result[property] == value) {
+          //Remove from array
+          array.splice(index, 1);
+          return;
+      }    
+   });
+}
